d7e186e29973609af494dfb66ced80c72a6a970d
EpicJhon <epicjhon1994@gmail.com>
2014-03-24 12:22:28 -0500

    enhance url recognition

--- framework/library/Utils.php
+++ framework/library/Utils.php
@@ -10,6 +10,26 @@ namespace iWorkPHP;
 class Utils {
 
     public function standardizeUrl($url) {
+
+        // Remove "http|https|ftp|...://", if any
+        if (strpos($url, '://') !== FALSE) {
+            $url = substr($url, strpos($url, '://') + 3); // Leave only "site.com/(...)"
+            $url = substr($url, strpos($url, '/') + 1); // Remove the first "/"
+        }
+
+        // Remove "//", if any
+        $url = (substr($url, 0, 2) == '//' ) ? substr($url, 2) : $url;
+
+        // Remove "./", if any
+        $url = (substr($url, 0, 2) == './' ) ? substr($url, 2) : $url;
+
+        // Remove index, if any
+        $url = (strpos($url, 'index.php') !== FALSE) ? substr($url, strpos($url, 'index.php') + 9) : $url;
+
+        // Add the first "/" if this does not exist
+        $url = (substr($url, 0, 1) !== '/' ) ? '/' . $url : $url;
+
+        // Put "/" at the end
         return (substr($url, -1) != '/') ? $url . '/' : $url;
     }
 
