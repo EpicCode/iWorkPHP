7f769efaee280cc937afca0d8a054e6db86aaeba
SamYan <luvel88@gmail.com>
2014-03-28 19:19:33 +0100

    added class description

--- framework/library/RouterRule.php
+++ framework/library/RouterRule.php
@@ -1,17 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace iWorkPHP;
 
 /**
- * Description of RouterRule
- *
- * @author Jhonjhon_123
+ * Router rule
  */
 class RouterRule {
 
