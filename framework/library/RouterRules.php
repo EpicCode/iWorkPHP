b7d4ba6235e1c61d235486877c6359400c8106a0
SamYan <luvel88@gmail.com>
2014-03-28 19:19:15 +0100

    class implementation to the "Iterator" interface

--- framework/library/RouterRules.php
+++ framework/library/RouterRules.php
@@ -1,24 +1,16 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace iWorkPHP;
 
 /**
- * Description of RouterRules
- *
- * @author Jhonjhon_123
+ * Routing rules manager
  */
-class RouterRules {
+class RouterRules implements \Iterator {
 
     private $rules;
 
     public function __construct() {
-        $this->rules = new \stdClass();
+        $this->rules = array();
     }
 
     public function getRules() {
@@ -26,15 +18,37 @@ class RouterRules {
     }
 
     public function hasRule($name) {
-        return property_exists($this->rules, $name);
+        return array_key_exists($name, $this->rules);
     }
 
     public function getRule($name) {
-        return $this->rules->$name;
+        return $this->rules[$name];
     }
 
-    public function setRule($name, $path, $pattern, $format, $className, $classMethod) {
-        $this->rules->$name = New RouterRule($path, $pattern, $format, $className, $classMethod);
+    public function addRule($name, $path, $pattern, $format, $className, $classMethod) {
+        if (!$this->hasRule($name))
+            $this->rules[$name] = New RouterRule($path, $pattern, $format, $className, $classMethod);
+    }
+
+    public function current() {
+        return current($this->rules);
+    }
+
+    public function key() {
+        return key($this->rules);
+    }
+
+    public function next() {
+        return next($this->rules);
+    }
+
+    public function rewind() {
+        reset($this->rules);
+    }
+
+    public function valid() {
+        $key = key($this->rules);
+        return ($key !== null && $key !== false);
     }
 
 }
