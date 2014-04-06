139be597da42699ecc44fe37fca9495130820a3a
EpicJhon <epicjhon1994@gmail.com>
2014-04-06 17:43:58 -0500

    fixed namespace

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
 // Get the composer autoload
 $composer = require_once __DIR__ . '/../vendor/autoload.php';
 // Call to framework loader
-$loader = new \iWorkPHP\Loader($composer);
+$loader = new \iWorkPHP\Core\Loader($composer);
 // handle actual request
 $loader->handleRequest();
 // Send response from actual request
