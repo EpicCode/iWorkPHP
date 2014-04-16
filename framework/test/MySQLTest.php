5c5fab329541a9c00fe4659a2a8305e42a9effda
EpicJhon <epicjhon1994@gmail.com>
2014-04-15 19:20:51 -0500

    added echo

--- framework/test/MySQLTest.php
+++ framework/test/MySQLTest.php
@@ -108,11 +108,13 @@ class MySQLTest extends PHPUnit_Framework_TestCase {
 
     public function testMappingDatabase() {
         $ret = $this->auxiliaryConsoleRunner($this->mappingDatabaseCommand);
+        echo $ret;
         $this->assertContains('mapping information to', $ret);
     }
 
     public function testGenerateEntitiesCommand() {
         $ret = $this->auxiliaryConsoleRunner($this->generateEntitiesCommand);
+        echo $ret;
         $this->assertContains('Entity classes generated to', $ret);
     }
 
